64231f2b52c241d8d4f988dc1655ada9a84d9bb5
SergeyNechaevCDO <Sergn@isever.net>
2018-12-03 11:06:28 +0300

    master merge new

--- app/src/main/java/com/isever/sergn/homeproject/MainActivity.java
+++ app/src/main/java/com/isever/sergn/homeproject/MainActivity.java
@@ -1,13 +1,15 @@
 package com.isever.sergn.homeproject;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import static com.isever.sergn.homeproject.R.*;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(layout.activity_main);
     }
 }
