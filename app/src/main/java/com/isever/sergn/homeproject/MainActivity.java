e9554fd40362c27d6586a48ed84f9cacad038221
SergeyNechaevCDO <Sergn@isever.net>
2019-01-31 13:13:20 +0300

    home work lesson 9

--- app/src/main/java/com/isever/sergn/homeproject/MainActivity.java
+++ app/src/main/java/com/isever/sergn/homeproject/MainActivity.java
@@ -1,21 +1,41 @@
 package com.isever.sergn.homeproject;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.isever.sergn.homeproject.controllers.MainMenu;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    //Не смог придумать как иначе работать с меню. Написыл костыль
     public final static MainMenu mainMenu = new MainMenu();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.app_name, R.string.app_name);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
     }
 
     @Override
@@ -27,5 +47,23 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return mainMenu.createOption(menu);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+        switch (id) {
+            case R.id.feedback:
+                Toast.makeText(this, "Форма обратной связи", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.program_info:
+                Toast.makeText(this, "Информация", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
